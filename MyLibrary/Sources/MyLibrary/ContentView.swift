afc4e6be5edfb9f2809c72e5e33a5a6e9a8a8d18
Jigar Thakkar <jigar@kintell.com>
2020-08-26 15:28:06 +1000

    SPM added

---
+++ MyLibrary/Sources/MyLibrary/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SPM
+//
+//  Created by Jigar on 26/8/20.
+//  Copyright © 2020 Kintell. All rights reserved.
+//
+
+import SwiftUI
+
+@available(iOS 13.0.0, *)
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+@available(iOS 13.0.0, *)
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
